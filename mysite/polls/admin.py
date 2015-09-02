900907778bc88f6b0769d8d27979dfe62e5bd810
Sarah Stander <sstander@astatine.cs.dixie.edu>
2015-09-02 17:36:23 -0600

    This is my second commit 9/2/15

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -9,9 +9,15 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['question_text', 'pub_date']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
     inlines = [ChoiceInline]
-
+    
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 
